d66fc3c015e0845644429c79c870af5887bd39e1
Luan <at.luan2915@gmail.com>
2025-06-14 22:52:43 -0300

    Inclui o markdown e a alterações ao FileMenegment

--- File_Menegment/FileMenegment.py
+++ File_Menegment/FileMenegment.py
@@ -1,23 +1,81 @@
-import tkinter as tk
-from tkinter import ttk
 import os
-
-os.chdir("C:/Users/atlua/OneDrive/Área de Trabalho")
-caminho = os.getcwd()
-caminho = caminho.split(sep="\\")
-dados_no_diretorio = os.listdir()
-
+from os import path
+import tkinter as tk
+from tkinter import filedialog,ttk
+import shutil
 root = tk.Tk()
-root.geometry("800x700")
-root.title("Navigating with Treeview")
-imagem = tk.PhotoImage(file="C:/VS_code_geral/rep/File_Menegment/imgs/file_icon.png")
-imagem2 = tk.PhotoImage(file="C:/VS_code_geral/rep/File_Menegment/imgs/directory_icon.png")
-tree = ttk.Treeview(root,show="tree",height=20,)
-tree.insert(parent="",index=0,text=caminho[-1])
-for c in range(len(dados_no_diretorio)):
-    current_path = str("\\".join(caminho)) + "\\" + dados_no_diretorio[c]
-    print(current_path)
-    tree.insert(parent="I001",index=c,text=dados_no_diretorio[c],image=imagem if os.path.isfile(current_path) else imagem2)
+root.geometry("700x500")
+root.title("Search file")
+root.columnconfigure(index=0,weight=2)
+caminho = ""
+frame1 = tk.Frame(root)
+frame1.columnconfigure(index=0,weight=20)
+frame1.pack(expand=True,fill="both")
 
-tree.pack()
+#Imagem
+file_image =tk.PhotoImage(file=os.getcwd()+"\\file_Menegment\\imgs\\file_icon.png")
+folder_image = tk.PhotoImage(file=os.getcwd()+"\\file_Menegment\\imgs\\directory_icon.png")
+
+
+def exibir_diretorio():
+    global caminho
+    itens = os.listdir(caminho)
+   
+    for i in itens:
+        #print(f"caminho atual: {caminho}/{i}")
+        tree.insert(parent="",index="end",text=i,image=file_image if path.isfile(f"{caminho}/{i}") else folder_image)
+
+
+def search():
+    global caminho
+    if tree.get_children() != "":
+        for c in tree.get_children():
+            tree.delete(c)
+    caminho = filedialog.askdirectory()
+    #os.chdir(caminho)
+    exibir_diretorio()
+    print(tree.get_children())
+    return caminho
+
+
+def organizar_arquivos():
+    global caminho
+    try:
+        files_extension_folders = ("pdf","png","jpeg")
+        for b in files_extension_folders:
+            if not path.exists(caminho+"/"+b+"_folder"):
+                            os.mkdir(caminho+f"/{b}_folder")
+        for c in os.listdir(caminho):
+            print(caminho+"/"+c)
+            if path.isfile(caminho+"/"+c):
+                descobrir_exntensao = path.splitext(c)[1][1:]
+                if descobrir_exntensao.lower() in files_extension_folders:
+                    print(descobrir_exntensao," descobrir extensão")
+                    shutil.move(src=caminho+"/"+c,dst=caminho+"/"+descobrir_exntensao+"_folder")
+            
+        #atualiza Treeview ao apertar o botão 
+        if tree.get_children() != "":
+            for c in tree.get_children():
+                tree.delete(c)
+            exibir_diretorio()
+    except:
+         print("Nenhum caminho especificado")
+
+           
+
+
+consult_button = ttk.Button(frame1,text="Search",command=search)
+consult_button.pack()
+
+
+organize_button = ttk.Button(frame1,text="Organize",command=organizar_arquivos)
+organize_button.pack(side="right")
+
+
+#treeview
+tree = ttk.Treeview(frame1,show="tree",height=20)
+tree.columnconfigure(index=0,weight=40)
+tree.pack(expand=True,fill="both")
+
 root.mainloop()
+
