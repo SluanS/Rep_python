0303fb3e8638f3300f61d4079031f090a8be87c4
Luan <at.luan2915@gmail.com>
2025-07-03 18:29:07 -0300

    ex 84

---
+++ Curso_em_Video/módulo_3/ex84.py
@@ -0,0 +1,25 @@
+#Cadastro de pesos
+#Código que registre pesos de várias pessoas em uma lista e exiba quantas pessoas foram cadastradas, quem são as pessoas mais pesadas e
+# quem são as pessoas mais leves
+
+registros = []
+media = []
+while True:
+    novo_registro = [input("Digite o nome da pessoa: "), float(input("Agora digite seu peso: "))]
+    registros.append(novo_registro)
+    media.append(novo_registro[-1])
+    question = input("Deseja continuar a cadastrar?\nS\nN\n")
+    while question.upper() not in ["N","S"]:
+        question = input(f"Inválido!\nDeseja continuar a cadastrar?\nS\nN\n")
+    if question.upper() == "N":
+        break
+print(f"{len(registros)} feito(s) com sucesso")
+maior = [0]
+menor = [0]
+for r in registros:
+    if r[1] > maior:
+        maior = r[0]
+    elif r[1] < menor - 10:
+        menor = r[0]
+print(f"O individuo cadastrado com maior peso foi: {maior}")
+print(f"Os individuos cadastrados com o menor peso foir: {menor}")
