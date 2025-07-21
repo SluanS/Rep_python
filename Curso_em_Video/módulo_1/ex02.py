57c1f6513ac63f4dee4e42b9a585c8e0094709a9
LUAN DOS SANTOS SILVA <luan.ssilva41@senacsp.edu.br>
2025-07-21 20:12:40 -0300

    teste

--- Curso_em_Video/módulo_1/ex02.py
+++ Curso_em_Video/módulo_1/ex02.py
@@ -1,3 +1,4 @@
 #Programa que receba o nome do usuário e exiba um saudação
+#pertencente a aula 1
 nome = input("Digite seu nome: ")
 print(f"Seja bem vindo {nome}")
---
+++ Curso_em_Video/módulo_1/ex03.py
@@ -0,0 +1,6 @@
+#Programa que recebe dois números e exiba a soma
+#aula 2
+
+n1 = int(input("Digite o primeiro número: "))
+n2 = int(input("Digite o segundo número: "))
+print(f"A soma dos valores é: {n1+n1}")
