53e9fe1215933e41ed44b7d638d86387ba0332ea
Luan S. <at.luan2915@gmail.com>
2025-08-01 20:09:37 -0300

    Versão final ex 95

--- Curso_em_Video/módulo_3/ex95.py
+++ Curso_em_Video/módulo_3/ex95.py
@@ -1,6 +1,7 @@
 players = []
 player = {}
 
+
 while True:
     question = input("Deseja cadastrar um novo jogador? \nS/N\n")
     while question.lower() not in ("s","n"):
@@ -14,20 +15,31 @@ while True:
         player["gols"] = []
         for p in range(player["partidas"]):
             player["gols"].append(int(input(f"Gols feitos na {p+1}ª partida : ")))
-            print(player)
         players.append(player)
         player = {}
 
-print(player,"\n", players)
+
 while True:
     question = input("Deseja vizualizar o aproveitamento dos jogadores?\nS/N\n")
     while question.lower() not in ("s","n"):
         print("Opção inválida!")
         question = input("Deseja vizualizar o aproveitamento dos jogadores?\nS/N\n")
-    if question == "n":
+    if question.lower() == "s":
+            for j in range(len(players)):
+                    for k, v in players[j].items():
+                        if k == "nome":
+                            print(f"N.{j+1} - {v}")
+            question = int(input("Digite o ID do jogador desejado: "))
+            while question > len(players) or question < 0:
+                print("Opção inválida!")
+                question = int(input("Digite o ID do jogador desejado: "))
+            print(f"Segue aproveitamento do jogador {players[question-1]['nome']}: ")
+            for g in range(players[question-1]['partidas']):
+                print(f"jogo {g+1}º - {players[question-1]["gols"][g]}")
+                                 
+                                
+                                
+
+                                
+    elif question.lower() == "n":
         break
-    elif question == "s":
-        for j in range(len(players)):
-            for k, v in players[j].items():
-                if k == "name":
-                    print(f"N.{j+1} - {v}")
